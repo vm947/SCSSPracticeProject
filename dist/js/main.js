a2add4d852588f591731caf145cc5e40edab2313
vm947 <vm947@nyu.edu>
2020-06-12 00:48:33 +0300

    changes

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,7 +2,11 @@ const menuBtn = document.querySelector(".menu-btn");
 const hamburger = document.querySelector(".menu-btn__burger");
 const nav = document.querySelector(".nav");
 const menuNav = document.querySelector(".menu-nav");
-
+const navItems = document.querySelectorAll(".menu-nav__item");
+//querySelector selects one element
+//querySelectorAll selects many and saves in array
+//so to appy a class to all items in array
+//we need to loop through them with forEach
 let showMenu = false;
 
 const toggleMenu = () => {
@@ -10,12 +14,14 @@ const toggleMenu = () => {
     hamburger.classList.add("open");
     nav.classList.add("open");
     menuNav.classList.add("open");
+    navItems.forEach((item) => item.classList.add("open"));
 
     showMenu = true;
   } else {
     hamburger.classList.remove("open");
     nav.classList.remove("open");
     menuNav.classList.remove("open");
+    navItems.forEach((item) => item.classList.remove("open"));
 
     showMenu = false;
   }
